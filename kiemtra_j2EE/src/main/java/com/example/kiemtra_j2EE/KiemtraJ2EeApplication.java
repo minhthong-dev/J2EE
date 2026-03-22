908349be9646ae167814f9ceb274b7e9600f893f
thonocodesiu <thongtg7@gmail.com>
2026-03-22 13:19:33 +0700

    kiem tra j2ee

---
+++ kiemtra_j2EE/src/main/java/com/example/kiemtra_j2EE/KiemtraJ2EeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiemtra_j2EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiemtraJ2EeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiemtraJ2EeApplication.class, args);
+	}
+
+}
---
+++ kiemtra_j2EE/src/test/java/com/example/kiemtra_j2EE/KiemtraJ2EeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiemtra_j2EE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiemtraJ2EeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
